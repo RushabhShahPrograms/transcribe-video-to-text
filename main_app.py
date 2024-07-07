a8e2309f20fb12d8e6551fa0d6fd088b79888a56
Rushabh Shah <90546286+RushabhShahPrograms@users.noreply.github.com>
2024-07-07 13:37:48 +0530

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,22 +1,26 @@
 import streamlit as st
-from pytube import YouTube
+import yt_dlp
 import os
 import whisper
 import torch
-import ffmpeg
 
-# Function to download YouTube video
-def download_youtube_video(url, download_path):
-    yt = YouTube(url)
-    stream = yt.streams.get_lowest_resolution()
-    video_path = stream.download(download_path)
-    return video_path, yt.title, yt.thumbnail_url
-
-# Function to convert video to audio using ffmpeg-python
-def convert_to_audio(video_path, audio_path):
-    stream = ffmpeg.input(video_path)
-    stream = ffmpeg.output(stream, audio_path, acodec='libmp3lame', ab='192k', ar='44100')
-    ffmpeg.run(stream, overwrite_output=True)
+# Function to download YouTube audio
+def download_youtube_audio(url, download_path):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+        'outtmpl': os.path.join(download_path, '%(title)s.%(ext)s')
+    }
+    
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=True)
+        audio_path = ydl.prepare_filename(info).rsplit(".", 1)[0] + ".mp3"
+    
+    return audio_path, info['title'], info['thumbnail']
 
 # Function to transcribe audio to text
 def transcribe_audio(audio_path, model_size="base"):
@@ -29,18 +33,14 @@ def transcribe_audio(audio_path, model_size="base"):
 st.title("YouTube Video to Text Transcriber")
 video_url = st.text_input("Enter YouTube Video URL")
 if video_url:
-    with st.spinner("Downloading video..."):
+    with st.spinner("Downloading audio..."):
         download_path = "./temp"
         os.makedirs(download_path, exist_ok=True)
-        video_path, video_title, thumbnail_url = download_youtube_video(video_url, download_path)
+        audio_path, video_title, thumbnail_url = download_youtube_audio(video_url, download_path)
     
     st.image(thumbnail_url, caption=video_title)
     
-    if st.button("Transcribe Video"):
-        audio_path = os.path.join(download_path, "audio.mp3")
-        with st.spinner("Converting video to audio..."):
-            convert_to_audio(video_path, audio_path)
-        
+    if st.button("Transcribe Audio"):
         with st.spinner("Transcribing audio to text..."):
             transcription = transcribe_audio(audio_path)
         
@@ -48,8 +48,5 @@ if video_url:
         
         if st.download_button("Download Transcription as TXT", transcription, file_name="transcription.txt"):
             # Clean up temporary files
-            os.remove(video_path)
             os.remove(audio_path)
             st.success("Temporary files deleted!")
-
-# Run the Streamlit app by executing `streamlit run script_name.py` from the command line
